5945d7c9a1fac0ece195f33c68f8330afb7ec22b
arianrdcas <arianrdcas@gmail.com>
2024-03-13 23:03:50 -0300

    Estilos y estructura HTML  para escritorio mejorados

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,16 @@ function App() {
     <>
       <img className="bgmobile" src={bgmobile} alt="Background" />
       <div className="container">
-        <img className="bg-card back" src={bgcardback} alt="Card Back" />
-        <p>000</p>
-        <div className="frontal">
+          <img className="bg-card back" src={bgcardback} alt="Card Back" />
+          <p>000</p>
           <img className="card front" src={bgcardfront} alt="Card Front" />
           <div className="circulo"></div>
           <div className="circulo dos"></div>
           <p className="credit_card_number">1234567891234567</p>
           <span className="nombre">Pepe Perez</span>
           <span className="fecha_expiracion">05/30</span>
-        </div>
       </div>
-      <Formulario /> 
+      <Formulario />
     </>
   );
 }
